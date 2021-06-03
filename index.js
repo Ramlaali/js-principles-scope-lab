02003c6e2584e1187cf72736e459d07c5ff2c25b
Ramla Abdullahi <ramlaali.abd@gmail.com>
2021-06-03 16:52:12 -0400

    commit tree 3c2167ecb8ad7faf1b70387e9e81f83e4a1f05d7

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+var customerName = `bob`;
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase(); 
+}
+
+//var bestCustomer = `not bob`
+function setBestCustomer(){
+    bestCustomer = `not bob`
+    return bestCustomer 
+
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = `maybe bob`
+    return bestCustomer
+}
+
+const leastFavoriteCustomer = `james`
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = `jessica`
+    return leastFavoriteCustomer
+    
+}
+
+
